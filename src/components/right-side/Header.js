95118932ff7cdada977f9c6beb4f6333fdb134c8
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-04-04 16:57:27 -0600

    added work done on monday

--- src/components/right-side/Header.js
+++ src/components/right-side/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import utdLogo from './manutd-logo-icon.jpg';
 import './css/Header.css';
 
 class Header extends Component {
@@ -8,13 +8,14 @@ class Header extends Component {
         return (
             <div className="header">
                 <div className="header-left">
-                    {/*<img src="{'./manutd-logo-icon.jpg'}" alt="Red Devils Logo" />*/}
+                    {/*<img src={require('./manutd-logo-icon.jpg')} alt="Red Devils Logo" />*/}
+                    <img src={utdLogo} alt="Red Devils Logo" />
                 </div>
                 <div className="header-center">
                     <h2>Manchester United FC</h2>
                 </div>
                 <div className="header-left">
-                    {/*<img src={logoUrl} alt="Red Devils Logo" />*/}
+                    <img src={utdLogo} alt="Red Devils Logo" />
                 </div>
             </div>
         );
