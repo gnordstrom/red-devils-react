4263ffd6f8feceb5b0eba61633516eb311c5c371
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-04-12 11:29:24 -0600

    added all previous work

--- src/components/views/home/Video.js
+++ src/components/views/home/Video.js
@@ -11,7 +11,7 @@ class VideoPlayer extends Component {
         }
         return (
             <div className="videoPlayer">
-                <iframe width="750" height="450" src="https://www.youtube.com/embed/g-3UaO1f7rQ" frameBorder="0" allowFullScreen></iframe>
+                <iframe className="youtube" width="750" height="450" src="https://www.youtube.com/embed/g-3UaO1f7rQ" frameBorder="0" allowFullScreen></iframe>
                 {/*<YouTube
                     videoId="g-3UaO1f7rQ" 
                     opts={opts}
